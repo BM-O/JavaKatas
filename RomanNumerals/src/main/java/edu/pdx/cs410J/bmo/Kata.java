b793530e95b96858f7433b05743531e11cd01e7a
Lawrence-Gunnell <36523041+Lawrence-Gunnell@users.noreply.github.com>
2020-07-08 20:30:55 -0700

    Add files via upload

--- RomanNumerals/src/main/java/edu/pdx/cs410J/bmo/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/bmo/Kata.java
@@ -15,9 +15,28 @@ public class Kata {
 
   public static String arabicToRoman(int num){
     String result = "I";
-
-
-    return result;
+    switch(num){
+      case 1:
+        return "I";
+      case 2:
+        return "II";
+      case 3:
+        return "III";
+      case 5:
+        return "V";
+      case 10:
+        return "X";
+      case 50:
+        return "L";
+      case 100:
+        return "C";
+      case 500:
+        return "D";
+      case 1000:
+        return "M";
+      default:
+        return "I";
+    }
   }
 
 }
