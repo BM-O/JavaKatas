7673177fb1ce103f0afb5cea93c441280225994d
Joseph Starr <starr5@pdx.edu>
2020-07-22 19:13:12 -0800

    stack method done. need test

--- rpn/src/main/java/edu/pdx/cs410J/bmo/Kata.java
+++ rpn/src/main/java/edu/pdx/cs410J/bmo/Kata.java
@@ -1,5 +1,9 @@
 package edu.pdx.cs410J.bmo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * A class for getting started with a code kata
  *
@@ -10,7 +14,20 @@ public class Kata {
                                                                                     
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
+//    System.err.println("Missing command line arguments");
+//    System.exit(1);
+
+    String expression = "20 5 /";
+    Stack<String> temp = getStack(expression);
+
   }
+
+  public static Stack<String> getStack(String expression) {
+    Stack<String> stackToReturn = new Stack<>();
+    stackToReturn.addAll(Arrays.asList(expression.split(" ")));
+    return stackToReturn;
+  }
+
+
+
 }
