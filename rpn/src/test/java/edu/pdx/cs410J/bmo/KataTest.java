aa20cc3de84424271674fefa23b8c89ad3323ca3
MQZON <bmo@pdx.edu>
2020-07-22 20:23:27 -0700

    Added getStackTurnsAStringIntoAStack test

--- rpn/src/test/java/edu/pdx/cs410J/bmo/KataTest.java
+++ rpn/src/test/java/edu/pdx/cs410J/bmo/KataTest.java
@@ -2,6 +2,11 @@ package edu.pdx.cs410J.bmo;
 
 import org.junit.Test;
 
+import java.util.Stack;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
 public class KataTest
 {
 
@@ -10,5 +15,11 @@ public class KataTest
     new Kata();
   }
 
+  @Test
+  public void getStackTurnsAStringIntoAStack(){
+    String testString = "20 5 /";
+    Stack<String> testStack = Kata.getStack(testString);
+    assertThat(testStack.size(), equalTo(3));
+  }
 
 }
